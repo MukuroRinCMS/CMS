4cd02b4d6acaa85bb674da3d5f9480e9600a647a
Rin42 <kiera42800@hotmail.fr>
2015-02-16 20:02:53 +0100

    First Commit

---
+++ php/header.php
@@ -0,0 +1,33 @@
+<header>
+<nav class="navbar navbar-inverse" role="navigation" style="border-radius: 0 0 0 0;">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./index.php">Acceuil</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Serveur</a></li>
+                <li><a href="#">Infos</a></li>
+                <li><a href="#">Compte</a></li>
+            </ul>
+                <li>
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="navbar-search form-control" placeholder="Recherchez">
+                        <button type="submit" class="btn btn-default glyphicon glyphicon-search"></button>
+                    </div>
+
+                </form>
+                </li>
+</nav>
+
+</header>
